ca0a0f369b68db300e640dbe81a548e905c9e012
Prawin panta <praveenpanta1082@gmail.com>
2021-01-04 21:07:27 +0545

    Routes issues resolved

--- routes/owner.route.js
+++ routes/owner.route.js
@@ -2,5 +2,8 @@ module.exports = app => {
     const owners = require('../controllers/owner.controller');
     var router = require('express').Router();
     router.post('/', owners.create);
+    router.get('/', owners.findAll);
+    router.get('/:id', owners.findOne);
+    router.put('/:id', owners.update)
     app.use('/api/owner', router);
 }
--- routes/renter.route.js
+++ routes/renter.route.js
@@ -2,5 +2,8 @@ module.exports = app => {
     const renter = require('../controllers/renter.controller');
     var router = require('express').Router();
     router.post('/', renter.create);
+    router.get('/', renter.findAll),
+        router.get('/:id', renter.findOne)
+    router.put('/:id', renter.update)
     app.use('/api/renter', router);
 }
