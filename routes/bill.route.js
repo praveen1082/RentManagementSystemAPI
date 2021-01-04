52ed09d7153629828e8b241785c79af8d8f1d441
Prawin panta <praveenpanta1082@gmail.com>
2021-01-04 21:04:50 +0545

    Workin

--- routes/bill.route.js
+++ routes/bill.route.js
@@ -2,5 +2,8 @@ module.exports = app => {
     const bill = require('../controllers/bill.controller');
     var router = require('express').Router();
     router.post('/', bill.create);
+    router.get('/', bill.findAll);
+    router.get('/:id', bill.findOne);
+    router.put('/:id', bill.update)
     app.use('/api/bill', router);
 }
--- routes/home.route.js
+++ routes/home.route.js
@@ -3,5 +3,8 @@ module.exports = app => {
     const home = require('../controllers/home.controller');
     var router = express.Router();
     router.post('/', home.create);
+    router.get('/:id', home.findOne);
+    router.get('/', home.findAll);
+    router.put('/:id', home.update)
     app.use('/api/home', router);
 }
