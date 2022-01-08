c50c817fe0b62174104644abdd8d2d3066b113ee
Prawin panta <praveenpanta1082@gmail.com>
2022-01-08 20:58:01 +0545

    working

--- controllers/bill.controller.js
+++ controllers/bill.controller.js
@@ -2,6 +2,93 @@ const db = require("../models");
 const Renter = db.renter;
 const Bill = db.bill;
 
+exports.findAll = (req, res) => {
+    Home.findAll().then(data => {
+        var dataList = data;
+        res.status(200).send({
+            code: 200,
+            response: {
+                message: "Successfully fetched all bills",
+                data: dataList
+            }
+        });
+    }).catch(err => {
+        res.status(500).send({
+            code: 500,
+            response: {
+                code: 500,
+                message: "Cannot fetch all bills"
+            }
+        })
+    })
+}
+exports.update = (req, res) => {
+    const id = req.params.billId;
+    // console.log(req.params.userId);
+    User.update(req.body, {
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.status(200).send({
+                    code: 200,
+                    response: {
+                        message: "bill updated successfully",
+                    }
+                });
+            } else {
+                res.status(500).send({
+                    code: 500,
+                    response: {
+                        message: `Cannot update bill with id=${id}. Maybe bill was not found or req.body is empty!`,
+                        data: null
+                    }
+                });
+
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                code: 500,
+                response: {
+                    message: `Cannot update bill with id=${id}. Maybe bill was not found or req.body is empty!`,
+                    data: err
+                }
+            });
+        });
+};
+exports.findOne = (req, res) => {
+    const id = req.params.billId;
+    Owner.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.status(200).send({
+                    code: 200,
+                    response: {
+                        message: "Success",
+                        data: data
+                    }
+                });
+            } else {
+                res.status(404).send({
+                    code: 404,
+                    response: {
+                        message: `Cannot find bill with id=${id}.`,
+                        data: null
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                code: 500,
+                response: {
+                    message: "Error retrieving bill with id=" + id,
+                    data: err
+                }
+            });
+        });
+};
 exports.create = (req, res) => {
     if (!req.body) {
         res.status(400).send({ code: 400, response: { message: "entire body cannot be empty", data: null } });
@@ -48,36 +135,5 @@ exports.create = (req, res) => {
     }).catch(err => {
         return res.status(500).send({ code: 500, response: { message: "error while creating bill", data: err } })
     })
-    exports.findOne = (req, res) => {
-        const id = req.params.renterId;
-        Owner.findByPk(id)
-            .then(data => {
-                if (data) {
-                    res.status(200).send({
-                        code: 200,
-                        response: {
-                            message: "Success",
-                            data: data
-                        }
-                    });
-                } else {
-                    res.status(404).send({
-                        code: 404,
-                        response: {
-                            message: `Cannot find bill with id=${id}.`,
-                            data: null
-                        }
-                    });
-                }
-            })
-            .catch(err => {
-                res.status(500).send({
-                    code: 500,
-                    response: {
-                        message: "Error retrieving bill with id=" + id,
-                        data: err
-                    }
-                });
-            });
-    };
+
 };
